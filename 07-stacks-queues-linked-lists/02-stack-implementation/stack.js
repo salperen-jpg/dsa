601c34ff2384f9b22252d037ea619b237d0cc2a7
salperen <sal.alperen1@gmail.com>
2024-10-29 23:55:21 +0100

    Add stack implementation

--- 07-stacks-queues-linked-lists/02-stack-implementation/stack.js
+++ 07-stacks-queues-linked-lists/02-stack-implementation/stack.js
@@ -1,3 +1,42 @@
-class Stack {}
+class Stack {
+  constructor() {
+    this.stack = [];
+    this.top = -1;
+  }
+
+  push(val) {
+    this.top++;
+    this.stack[this.top] = val;
+    return true;
+  }
+
+  pop() {
+    if (this.top === -1) return;
+    const poppedValue = this.stack.pop();
+    this.top--;
+    return poppedValue;
+  }
+
+  peek() {
+    if (this.top !== -1) return this.stack[this.top];
+  }
+
+  isEmpty() {
+    return this.top === -1;
+  }
+}
+
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+stack.push(5);
+stack.pop();
+stack.pop();
+console.log(stack.peek());
+console.log(stack.isEmpty());
+stack.pop();
+console.log(stack);
 
 module.exports = Stack;
